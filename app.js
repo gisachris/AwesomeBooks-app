3a56576c4b1d39575a0f69270f374306ad694823
gisa chris <gisachris24@gmail.com>
2023-05-04 09:58:53 -0200

    single page implementation

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // displaying all the books
     for (let i = 0; i < bookHolder.length; i++) {
       const bookInstance = document.createElement('article');
+      bookInstance.classList.add('dercounter');
       const dispTitle = document.createElement('h2');
       dispTitle.innerHTML = bookHolder[i].title;
       const dispAuthor = document.createElement('p');
@@ -92,10 +93,13 @@ document.addEventListener('DOMContentLoaded', () => {
   addButton.addEventListener('click', () => {
     // validate before adding
     if (inputTitle.value === '' || inputAuthor.value === '') {
-      bookDisplay.append(error);
+      form.append(error);
+      form.insertBefore(error, inputTitle);
+      error.style.display = 'block';
       inputTitle.value = '';
       inputAuthor.value = '';
     } else {
+      error.style.display = 'none';
       addBook();
       bookDisplay.innerHTML = '';
       display();
@@ -119,13 +123,20 @@ document.addEventListener('DOMContentLoaded', () => {
   //tab buttons
   const listTab = document.querySelector('.list');
   const addingTab = document.querySelector('.adding');
-  const contactTab = document.querySelector('.contact');
+  const contactTab = document.querySelector('.cont');
 
   //the h2 tag
-  const titletext = document.querySelector('.Pagetitle .titletext');
+  const titletext = document.querySelector('.titletext');
 
   //the contact page
-  const contactpage = document.querySelector('.container div .contact'); 
+  const contactpage = document.querySelector('.contact'); 
+
+  //mssg for no books
+  const bookarticles = document.querySelectorAll('.dercounter');
+  const directive =document.createElement('span');
+  directive.classList.add('directive');
+  directive.textContent = 'No books have been added yet';
+  bookDisplay.append(directive);
 
   function listOn(){
     //changing the h2
@@ -135,7 +146,13 @@ document.addEventListener('DOMContentLoaded', () => {
     bookDisplay.style.display = 'block';
     form.style.display = 'none';
     contactpage.style.display = 'none';
- 
+
+    //when no books have been added yet
+    if(bookarticles.length>0){
+      directive.style.display = 'none';
+    }else {
+      directive.style.display = 'block';
+    }
  
      //changing the colors
      if(bookDisplay.style.display === 'block'){
@@ -155,12 +172,12 @@ document.addEventListener('DOMContentLoaded', () => {
  
     //removing other sections
     bookDisplay.style.display = 'none';
-    form.style.display = 'block';
+    form.style.display = 'flex';
     contactpage.style.display = 'none';
  
  
      //changing the colors
-     if(bookDisplay.style.display === 'none'){
+     if(form.style.display === 'flex'){
       addingTab.style.color = 'blue';
       listTab.style.color = 'black';
       contactTab.style.color = 'black';
@@ -181,9 +198,12 @@ document.addEventListener('DOMContentLoaded', () => {
     form.style.display = 'none';
     contactpage.style.display = 'block';
  
+    //arrange elements
+    const titlesection = document.querySelector('.Pagetitle');
+    container.insertBefore(titlesection, contactpage);
  
      //changing the colors
-     if(contactTab.style.display === 'block'){
+     if(contactpage.style.display === 'block'){
       addingTab.style.color = 'black';
       listTab.style.color = 'black';
       contactTab.style.color = 'blue';
@@ -195,7 +215,47 @@ document.addEventListener('DOMContentLoaded', () => {
   
    contactTab.addEventListener('click',contactOn);
 
-  //the body 
-  // const body = document.body;
-  // body.addEventListener('onload',addOn);
+  //insert the time 
+  function time() {
+    const myDate = new Date();
+    const year = myDate.getFullYear();
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const monthindex = myDate.getMonth();
+    const month = monthNames[monthindex];
+    const day = myDate.getDate();
+    let daySuffix;
+  switch (day % 10) {
+    case 1:
+      daySuffix = 'st';
+      break;
+    case 2:
+      daySuffix = 'nd';
+      break;
+    case 3:
+      daySuffix = 'rd';
+      break;
+    default:
+      daySuffix = 'th';
+      break;
+  }
+    const formatteddays = day < 10 ? '0' + day : day;
+    const dayadder = month + '-' + formatteddays + daySuffix + '-' + year;
+    const hours = myDate.getHours();
+    const minutes = myDate.getMinutes();
+    const seconds = myDate.getSeconds();
+    const ampm = hours >= 12 ? 'pm' : 'am';
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+    const formattedTime = formattedHours + ':' + formattedMinutes + ':' + formattedSeconds + ' ' + ampm;
+    return dayadder + ' ' + formattedTime;
+  }
+  
+  const datetime = document.querySelector('.datetime');
+  datetime.textContent = time();
+  
+
+  //when the page loads
+  window.addEventListener('load',listOn);
+
 });
